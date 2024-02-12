974e1787c2eb4124c277c9f7307bab8be33d872e
Mahnoor Gul <96388430+mahnoorgullper@users.noreply.github.com>
2024-02-12 18:47:25 +0500

    added config files

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    
+  },
+  
+  chromeWebSecurity: false,
+  
+    "reporter": "mochawesome",
+    "reporterOptions": {
+      "reportDir": "cypress/reports/mocha",
+      "overwrite": false,
+      "html": false,
+      "json": true
+    }
+  
+});
