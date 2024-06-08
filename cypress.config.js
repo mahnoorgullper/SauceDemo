d1cd94a58ba8a75b1a40aeffa6decd287ed4f581
mahnoorgullper <mahnoorgul288@gmail.com>
2024-06-08 23:31:03 +0500

    removed config.js file and added baseurl in cypress.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      baseUrl: 'https://www.saucedemo.com/',
+    }
   },
 
   chromeWebSecurity: false,
